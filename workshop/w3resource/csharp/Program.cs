23ea5a9f90a25dc969bf5285055d90884f11cc14
Frend Fernando <Frend@hdlcompanies.com>
2019-11-11 11:59:02 -0800

    w3r init

---
+++ workshop/w3resource/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(e1(3, 2));
+      Console.WriteLine(e1(2, 2));
+      Console.ReadLine();
+    }
+    static int e1(int x, int y)
+    {
+      // method that compute the sum of the two given integer values.
+      // if the two values are the same, then return triple their sum.
+      return x == y ? (x + y) * 3 : x + y;
+    }
+  }
+}
---
+++ workshop/w3resource/javascript/w3r.js
@@ -0,0 +1,8 @@
+// 1 program to compare two objects to determine if the first one contains equivalent property values to the second one
+let matches = (obj, source) =>
+  Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
+
+console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
+console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
+// -
+
