532ea9dd74a76f47269a8e5888965b8dded0f7c3
Frend Fernando <Frend@hdlcompanies.com>
2019-11-14 11:45:01 -0800

    w3r e2

--- workshop/w3resource/csharp/Program.cs
+++ workshop/w3resource/csharp/Program.cs
@@ -6,8 +6,11 @@ namespace csharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine(e1(3, 2));
-      Console.WriteLine(e1(2, 2));
+      // examples 
+      Console.WriteLine(e1(3, 2)); // 5
+      Console.WriteLine(e1(2, 2)); // 12
+      Console.WriteLine(e2(30)); // 21
+      Console.WriteLine(e2(51)); // 0
       Console.ReadLine();
     }
     static int e1(int x, int y)
@@ -15,6 +18,15 @@ namespace csharp
       // method that compute the sum of the two given integer values.
       // if the two values are the same, then return triple their sum.
       return x == y ? (x + y) * 3 : x + y;
+    }
+    static int e2(int n)
+    {
+      const int x = 51;
+      if (n > x)
+      { 
+        return (n - x) * 3;
+      }
+      return x - n;
     }
   }
 }
--- workshop/w3resource/javascript/w3r.js
+++ workshop/w3resource/javascript/w3r.js
@@ -1,4 +1,4 @@
-// 1 program to compare two objects to determine if the first one contains equivalent property values to the second one
+// 1 method to compare two objects to determine if the first one contains equivalent property values to the second one
 let matches = (obj, source) =>
   Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
 
@@ -6,3 +6,14 @@ console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', bear
 console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
 // -
 
+// 2 method that converts a comma separated values (CSV) to string to a 2D array
+const csv_to_array = (data, delimeter = ',', omitFirstRow = false) =>
+  data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map(v => v.split(delimeter));
+
+console.log(csv_to_array('a,b\nc,d')); // [["a","b"],["c","d"]]
+console.log(csv_to_array('a;b\nc;d', ';')); // [["a","b"],["c","d"]]
+console.log(csv_to_array('head1,head2\na,b\nc,d', ',', true)); // [["a","b"],["c","d"]]
+// -
